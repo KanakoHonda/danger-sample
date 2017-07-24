4fc3d33eebeadb3582945a83504eb1a171892a47
KanakoHonda <kanakohonda251@gmail.com>
2017-07-24 11:51:50 +0900

    Delte toast

--- DangerSample/app/src/main/java/com/example/dangersample/MainActivity.java
+++ DangerSample/app/src/main/java/com/example/dangersample/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "test", 0).show();
-        Toast.makeText(this, "test", 0).show();
-        Toast.makeText(this, "test", 0).show();
     }
 }
